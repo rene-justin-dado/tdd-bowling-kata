aaaff254b290af6e919602f7f9aab2a68e703abc
EDA <eda@mac-mini-3.local>
2016-07-21 10:07:08 +1200

    gutterball tests

--- tests/index.js
+++ tests/index.js
@@ -1,6 +1,15 @@
 var test = require('tape')
+var game = require('../game')
 
 test('initial', function (t) {
-  t.plan(1)
-  t.equal('a', 'a')
+  t.plan(1) // Can be done with t.pass
+  t.equal('a', 'a') // Does the test
+})
+
+test('scores a gutterball frame', function (t) {
+  var frame = [0, 0]
+  var expected = 0
+  var actual = game.scoreFrame(frame)
+  t.equal(actual, expected)
+  t.end()
 })
