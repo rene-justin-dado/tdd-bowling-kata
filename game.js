d6c632f4ccfcece10d6a93d92ec054d54dcd8401
EDA <eda@mac-mini-3.local>
2016-07-21 11:51:48 +1200

    added function for single strike

--- game.js
+++ game.js
@@ -6,5 +6,8 @@ function scoreFrame (frame1, frame2) {
   if (frame1[0] + frame1[1] === 10 && frame1[0] < 10) {
     return frame1[0] + frame1[1] + frame2[0]
   }
+  if (frame1[0] === 10) {
+    return frame1[0] + frame2[0] + frame2[1]
+  }
   return frame1[0] + frame1[1]
 }
