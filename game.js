3736b052f9e456db5959f501ddc001a0266c5e4f
EDA <eda@mac-mini-3.local>
2016-07-21 11:25:26 +1200

    modifying scoreFrame to check spares

--- game.js
+++ game.js
@@ -2,6 +2,9 @@ module.exports = {
   scoreFrame: scoreFrame
 }
 
-function scoreFrame (frame) {
-  return frame[0] + frame[1]
+// var frame = ([7, 3], [3, 5])
+// var expected = 13
+function scoreFrame (frame1, frame2) {
+  return frame1[0] + frame1[1]
+  
 }
