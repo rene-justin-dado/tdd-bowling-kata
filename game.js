a06e126a7f6f2c62720fe9562a557a169ab168dc
EDA <eda@mac-mini-3.local>
2016-07-21 18:46:09 +1200

    work complete

--- game.js
+++ game.js
@@ -10,13 +10,18 @@ function scoreGame (frames) {
   var score = 0
   var i = 0
   while (i < frames.length) {
-    if (isStrike(frames[i]) || isSpare(frames[i])) {
-      score += scoreSpecial(frames[i], frames[i + 1], frames[i + 2])
-      console.log(score)
+    if (i === 9) {
+      score += frames[9].reduce(function(a, b) {
+        return a + b
+      })
     } else {
-      score += scoreFrame(frames[i])
-      console.log(score)
+       if (isStrike(frames[i]) || isSpare(frames[i])) {
+        score += scoreSpecial(frames[i], frames[i + 1], frames[i + 2])
+      } else {
+        score += scoreFrame(frames[i])
+      }
     }
+
     i++
   }
   return score
@@ -38,19 +43,14 @@ function scoreFrame (frame) {
 function scoreSpecial (frame1, frame2, frame3) {
   //Multi Strike
   if (isStrike(frame1) && isStrike(frame2)) {
-    console.log('Multi Strike')
     return frame1[0] + frame2[0] + frame3[0]
   }
   //Single Strike
   if (isStrike(frame1)) {
-    console.log('Single Strike')
     return frame1[0] + frame2[0] + frame2[1]
   }
   //Spare
   if (isSpare(frame1)) {
-    console.log('Spare')
     return frame1[0] + frame1[1] + frame2[0]
   }
 }
-
-// scoreGame([[10, 0], [10, 0], [10, 0], [3, 2], [1, 9], [4, 6], [5, 3], [0, 8], [0, 0], [0, 0]])
